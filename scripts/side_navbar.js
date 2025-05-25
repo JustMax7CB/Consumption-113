96af2690ecc006f954196a7fd245b8d45cf48353
JustMax7CB <itsjustmax9@gmail.com>
2025-05-25 16:56:43 +0300

    Created side navbar and logout mechanism

--- scripts/side_navbar.js
+++ scripts/side_navbar.js
@@ -1,3 +1,11 @@
+window.addEventListener("DOMContentLoaded", () => {
+  const isAdmin = localStorage.getItem("adminUnlocked") === "true";
+  const logoutBtn = document.querySelector("#logoutBtn");
+  if (logoutBtn) {
+    logoutBtn.style.display = isAdmin ? "block" : "none";
+  }
+});
+
 function toggleSideNav() {
   const sidenav = document.querySelector("#mySidenav");
   const hamburger = document.querySelector(".hamburger");
@@ -13,16 +21,40 @@ function toggleSideNav() {
   }
 }
 
-function checkCredentials() {
-  const ADMIN_CODE = process.env.NEXT_PUBLIC_ADMIN_CODE || "1234";
+const  checkCredentials = async () => {
+  const adminUnlocked = localStorage.getItem("adminUnlocked");
 
-  console.log(ADMIN_CODE);
-  if (window.location.href === "admin.html") return;
-  const password = prompt("אנא הזן סיסמה:");
-
-  if (password === "1234") {
-    window.location.href = "admin.html"; // Redirect if correct
-  } else {
-    alert("סיסמה שגויה");
+  if (adminUnlocked === "true") {
+    window.location.href = "admin.html";
+    return;
   }
+
+  const STORED_HASH = "1520337df654eec274e41789d187ea2b15939bfcdb312ed90364f76258c91037";
+
+  const input = prompt("הזן סיסמה:");
+    if (!input) return alert("סיסמה לא הוזנה");
+    const inputHash = await hashPassword(input);
+
+    if (inputHash === STORED_HASH) {
+      alert("כניסה מאושרת!");
+      localStorage.setItem("adminUnlocked", "true");
+      window.location.href = "admin.html";
+    } else {
+      alert("סיסמה שגויה");
+    }
+}
+
+const adminLogout = () => {
+  localStorage.setItem("adminUnlocked", "false");
+  alert("התנתקת בהצלחה!")
+  location.reload();
+  window.location.href = "index.html";
+}
+
+const hashPassword = async (password) => {
+  const msgBuffer = new TextEncoder().encode(password);
+  const hashBuffer = await crypto.subtle.digest('SHA-256', msgBuffer);
+  const hashArray = Array.from(new Uint8Array(hashBuffer));
+  const hashHex = hashArray.map(b => b.toString(16).padStart(2, '0')).join('');
+  return hashHex;
 }
